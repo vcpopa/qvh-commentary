1f5bbe8b96ec955ec2c49feeca58eb2a8883c678
EveLarsenRWH <141052625+EveLarsenRWH@users.noreply.github.com>
2024-07-03 18:19:37 +0100

    Added Missing CommentaryLevel column

--- src/main.py
+++ src/main.py
@@ -72,7 +72,7 @@ if __name__ =="__main__":
         filtered_df = df_Table[df_Table['CommentaryBox'] == box]
         # Pivot the filtered DataFrame
         pivot_df = filtered_df.pivot_table(
-            index=['Area_ID', 'CommentarySection', 'CommentaryBox', 'OrderNo', 'Measure_Description', 'Period', 'LastSaved'],
+            index=['Area_ID', 'CommentarySection', 'CommentaryBox', 'OrderNo', 'Measure_Description', 'Period', 'LastSaved', 'CommentaryLevel'],
             columns='ColumnTitle',
             values='ColumnValue',
             aggfunc='first'
@@ -87,7 +87,7 @@ if __name__ =="__main__":
             for goal in unique_annual_goals:
                 goal_filtered_df = filtered_df[filtered_df['AnnualGoal'] == goal]
                 goal_pivot_df = goal_filtered_df.pivot_table(
-                    index=['Area_ID', 'CommentarySection', 'CommentaryBox', 'OrderNo', 'Measure_Description', 'Period', 'LastSaved'],
+                    index=['Area_ID', 'CommentarySection', 'CommentaryBox', 'OrderNo', 'Measure_Description', 'Period', 'LastSaved','CommentaryLevel'],
                     columns='ColumnTitle',
                     values='ColumnValue',
                     aggfunc='first'
